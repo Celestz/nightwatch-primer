79bead3f2905d555a7a18106a4941e0b8f4d0ff4
Patrick Chua <i.am.celestz@gmail.com>
2015-03-30 17:37:12 +0800

    Step 2 Cambogia - Checkout Process

--- examples/tests/cambogia.js
+++ examples/tests/cambogia.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "Fill Cambogia" : function (browser) {
+  "Fill Out Index" : function (browser) {
     browser
       .url("http://open:nutra@katm.us/adshore/feature/thepurecambogiaultra.com/sea")
       .waitForElementPresent('form[name="f"]',1000)
@@ -9,16 +9,28 @@ module.exports = {
       .setValue('input[name="address2"]', 'Address 2')
       .setValue('input[name="city"]', 'Makati')
       .setValue('input[name="address2"]', 'Address 2')
-      .setValue('select[name="state"]', '64')
+      .click('select[name="state"] option[value="64"]')
       .setValue('input[name="zip"]', '11111')
       .setValue('input[name="phone"]', '123456789')
       .setValue('input[name="email"]', 'patrick@katmus.com')
       .click('form[name="f"] .btn-rush')
-      .waitForElementNotVisible('.modal-backdrop.in',1000)
+      .waitForElementNotVisible('.modal-backdrop.in',500)
       .assert.urlContains('checkout')
       // .waitForElementVisible('#todoExample button', 1000)
       // .click('#todoExample input')
       // .assert.containsText('#main', 'The Night Watch')
-      .end();
+  },
+  "Checkout Single Bottle Package" : function(browser) {
+    browser
+    .click('.boxes.box3')
+    .click('select[name="payment_method"] option[value="visa"]')
+    .setValue('input[name="cc_number"]','4444333322221111')
+    .click('select[name="cc_exp_mm"] option[value="05"]')
+    .click('select[name="cc_exp_yyyy"] option[value="2017"]')
+    .setValue('input[name="cc_csc"]','125')
+    .click('form[name="f"] .btn-rush')
+    .waitForElementNotVisible('.modal-backdrop.in',4000)
+    .assert.urlContains('upsell')
+    .end();
   }
 };
